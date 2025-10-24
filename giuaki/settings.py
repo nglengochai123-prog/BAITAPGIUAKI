e229064635f96e4939f000754e7b441d6f85f509
John Doe <nglengochai123@gmail.com>
2025-10-24 10:54:19 +0700

    ta them admin r

--- giuaki/settings.py
+++ giuaki/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Khenthuong',
+    'Quanlyhoso',
+    'Quanlytuyendung',
+    'Tinhluong'
 ]
 
 MIDDLEWARE = [
